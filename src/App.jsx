61d57a81be64c910030f976f3e3be9a7cd13dbb2
Demo Acc <shabiransari0000@gmail>
2026-05-19 14:25:18 +0530

    added src file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="dad"> <h1>Hello whats's up</h1> </div>
+    <div id="uncle"> <h2>Hi what's going on i hope everything  is fine </h2> </div>
+    </>
+  )
+}
+
+export default App
